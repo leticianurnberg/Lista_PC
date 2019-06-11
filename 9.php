43c363567ed2532b3377573a5710768a24e0e534
leticianurnberg <48133429+leticianurnberg@users.noreply.github.com>
2019-06-11 20:10:56 -0300

    Create 9.php

---
+++ 9.php
@@ -0,0 +1,19 @@
+<?php
+
+function divisivel ($x,$y) {
+        $modulo = $x%$y;
+        if ($modulo == 0) {
+            return "1 \n";
+        }
+        else {
+            return "0 \n";
+        }
+}
+
+print "Digite um número: ";
+$x = (int)fgets(STDIN);
+
+print "Digite outro número: ";
+$y = (int)fgets(STDIN);
+
+print divisivel ($x,$y);
